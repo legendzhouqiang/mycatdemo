89d4b15e4fc71649b6b9bb0daf99c1cfacc4a65d
qinchy <qinchy@msn.com>
2017-08-29 11:18:11 +0800

    添加工程的骨架

---
+++ mycatdemo-biz/src/main/java/com/qinchy/mycatdemobiz/MycatdemoBizApplication.java
@@ -0,0 +1,12 @@
+package com.qinchy.mycatdemobiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycatdemoBizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycatdemoBizApplication.class, args);
+	}
+}
