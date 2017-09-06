f4d402c59821e1c2b17b62312acf6871e4ee6a5a
qinchy <qinchy@msn.com>
2017-09-06 10:49:44 +0800

    添加mycat的三个重要配置文件

--- mycatdemo-api/src/main/java/com/qinchy/mycatdemo/service/impl/CustomerServiceImpl.java
+++ mycatdemo-api/src/main/java/com/qinchy/mycatdemo/service/impl/CustomerServiceImpl.java
@@ -6,10 +6,12 @@ import com.qinchy.mycatdemo.model.CustomerExample;
 import com.qinchy.mycatdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
