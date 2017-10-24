e8f2d3e9f2882090e991e2c3ab23f476d7bbdb4a
qinchy <qinchy@msn.com>
2017-10-24 14:57:27 +0800

    修改了初始化脚本和rule和schema配置

--- init_mycat.sql
+++ init_mycat.sql
@@ -365,3 +365,56 @@ CREATE TABLE `travelrecord` (
 -- ----------------------------
 -- Records of travelrecord
 -- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_range_date`
+-- ----------------------------
+ CREATE TABLE `t_range_date` ( 
+	`id` INT NOT NULL, 
+	`date` DATE NULL, 
+	`date_str` VARCHAR(45) NULL, 
+	`context` VARCHAR(45) NULL, PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_range_date
+-- ----------------------------
+INSERT INTO t_range_date (id,date_str,context) VALUES(1,'2015-01-01','month-1-str');
+INSERT INTO t_range_date (id,date_str,context) VALUES(2,'2015-02-01','month-2-str');
+INSERT INTO t_range_date (id,date_str,context) VALUES(3,'2015-03-01','month-3-str');
+INSERT INTO t_range_date (id,date_str,context) VALUES(4,'2015-04-01','month-4-str');
+INSERT INTO t_range_date (id,date_str,context) VALUES(5,'2015-05-01','month-5-str');
+INSERT INTO t_range_date (id,date_str,context) VALUES(6,'2015-06-01','month-6-str');
+INSERT INTO t_range_date (id,date_str,context) VALUES(7,'2015-07-01','month-7-str');
+INSERT INTO t_range_date (id,date_str,context) VALUES(8,'2015-08-01','month-8-str');
+INSERT INTO t_range_date (id,date_str,context) VALUES(9,'2015-09-01','month-9-str');
+INSERT INTO t_range_date (id,date_str,context) VALUES(10,'2015-10-01','month-10-str');
+INSERT INTO t_range_date (id,date_str,context) VALUES(11,'2015-11-01','month-11-str');
+INSERT INTO t_range_date (id,date_str,context) VALUES(12,'2015-12-01','month-12-str');
+
+-- ----------------------------
+-- Table structure for `msg`
+-- ----------------------------
+CREATE TABLE `msg` (
+  `id` BIGINT(20) NOT NULL,
+  `content` VARCHAR(4000) DEFAULT NULL,
+  `create_time` DATETIME DEFAULT NULL COMMENT '创建时间',
+  `create_date` INT(8) DEFAULT NULL COMMENT '按月分表字段，不能为空。',
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of msg
+-- ----------------------------
+INSERT INTO `msg` (`id`, `content`, `create_time`, `create_date`) VALUES('1','content1','2017-01-29 16:11:59','20170129');
+INSERT INTO `msg` (`id`, `content`, `create_time`, `create_date`) VALUES('2','content2','2017-02-28 16:11:59','20170228');
+INSERT INTO `msg` (`id`, `content`, `create_time`, `create_date`) VALUES('3','content3','2017-03-29 16:11:59','20170329');
+INSERT INTO `msg` (`id`, `content`, `create_time`, `create_date`) VALUES('4','content4','2017-04-29 16:11:59','20170429');
+INSERT INTO `msg` (`id`, `content`, `create_time`, `create_date`) VALUES('5','content5','2017-05-29 16:11:59','20170529');
+INSERT INTO `msg` (`id`, `content`, `create_time`, `create_date`) VALUES('6','content6','2017-06-29 16:11:59','20170629');
+INSERT INTO `msg` (`id`, `content`, `create_time`, `create_date`) VALUES('7','content7','2017-07-29 16:11:59','20170729');
+INSERT INTO `msg` (`id`, `content`, `create_time`, `create_date`) VALUES('8','content8','2017-08-29 16:11:59','20170829');
+INSERT INTO `msg` (`id`, `content`, `create_time`, `create_date`) VALUES('9','content9','2017-09-29 16:11:59','20170929');
+INSERT INTO `msg` (`id`, `content`, `create_time`, `create_date`) VALUES('10','content10','2017-10-29 16:11:59','20171029');
+INSERT INTO `msg` (`id`, `content`, `create_time`, `create_date`) VALUES('11','content11','2017-11-29 16:11:59','20171129');
+INSERT INTO `msg` (`id`, `content`, `create_time`, `create_date`) VALUES('12','content12','2017-12-29 16:11:59','20171229');
